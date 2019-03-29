c253cd0b67959b3940a3274e512cc734fc0f4bcc
Corey Zumar <corey.zumar@databricks.com>
2019-03-28 22:36:04 -0700

    Add metric

--- train.py
+++ train.py
@@ -101,7 +101,6 @@ def train_model(model):
             outputs = model(images)
             loss = criterion(outputs, labels)
 
-
             # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
@@ -113,19 +112,21 @@ def train_model(model):
                 print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                        .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
-        # Test the model
-        with torch.no_grad():
-            correct = 0
-            total = 0
-            for images, labels in test_loader:
-                images = images.reshape(-1, sequence_length, input_size).to(device)
-                labels = labels.to(device)
-                outputs = model(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                # Test the model
+                with torch.no_grad():
+                    correct = 0
+                    total = 0
+                    for images, labels in test_loader:
+                        images = images.reshape(-1, sequence_length, input_size).to(device)
+                        labels = labels.to(device)
+                        outputs = model(images)
+                        _, predicted = torch.max(outputs.data, 1)
+                        total += labels.size(0)
+                        correct += (predicted == labels).sum().item()
 
-        print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+                mlflow.log_metric("Test accuracy vs number of training epochs", 100 * correct / total)
+
+                print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
 
 model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
 if args.checkpoint_path:
